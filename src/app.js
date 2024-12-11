3440689c95197f2d01ee55ef066a0397bc419b79
thanhthu2 <89637761+thanhthu2@users.noreply.github.com>
2024-12-11 17:24:54 +0700

    feat: Section 2: Node.js Backend Architecture - Những folders và packages cần thiết khi khởi tạo Project!

---
+++ src/app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const morgan = require("morgan"); // dùng để in log
+const helmet = require("helmet"); // dùng để bảo vệ header
+const compression = require("compression"); //tối ưu size
+
+const app = express();
+
+app.use(morgan("dev"));
+// morgan("combined"); kiểu log
+// morgan("common"); kiểu log
+// morgan("short"); kiểu log
+// morgan("tiny"); kiểu log
+app.use(helmet());
+app.use(compression());
+//init middlewares
+
+//init db
+
+//init routes
+app.get("/", (req, res, next) => {
+  const strCompress = " Hello Factipjs";
+  return res.status(200).json({
+    message: "Welcome Fantipjs",
+    metadata: strCompress.repeat(10000),
+  });
+});
+
+//handling error
+module.exports = app;
