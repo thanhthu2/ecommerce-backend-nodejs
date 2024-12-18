494586f37f5bb187623efff9f5a4e2579bfe1430
thanhthu2 <89637761+thanhthu2@users.noreply.github.com>
2024-12-18 16:37:58 +0700

    feat: Section 7: Xử lý ErrorHandler trong API | Course Node.js Backend Architecture

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -2,9 +2,7 @@ const AccessService = require("../services/access.service");
 
 class AccessController {
   signUp = async (req, res, next) => {
-    try {
-      return res.status(200).json(await AccessService.signUp(req.body));
-    } catch (error) {}
+    return res.status(200).json(await AccessService.signUp(req.body));
   };
 }
 
